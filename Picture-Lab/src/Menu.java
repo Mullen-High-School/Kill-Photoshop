d5c028b9624316f9e7e0a6777ef8ef6cca086420
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2019-05-10 08:24:36 -0600

    don't do amything with the JFrame

--- Picture-Lab/src/Menu.java
+++ Picture-Lab/src/Menu.java
@@ -1,8 +1,22 @@
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Menu {
 
+	
+		static JFrame frame = new JFrame("Universal Converter v.1");
 	public static void main(String[] args) {
 	    
+		displayJFrame();
+		
 		//PictureTester.testZeroBlue();
 	    //PictureTester.keepOnlyBlue();
 		//PictureTester.mirrorDiagonal();
@@ -17,7 +31,7 @@ public class Menu {
 	    //PictureTester.testGrayscale();
 	    //PictureTester.fixUnderwater();
 	    //PictureTester.testMirrorVertical();
-		PictureTester.mirrorHorizontal();   //added
+		//PictureTester.mirrorHorizontal();   //added
 		//PictureTester.mirrorHorizontalBotToTop();
 	    //PictureTester.mirrorVerticalRightToLeft();
 	    //PictureTester.testMirrorTemple();
@@ -35,7 +49,77 @@ public class Menu {
 	    //PictureTester.testClearBlueOverValue(200);
 	    //PictureTester.testGetAverageForColumn(0);
 	  
+		
+		
+		
+		
+		
+	}
+			
+		public static void displayJFrame()
+		{
+			
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(300, 150);
+			frame.setLocation(430, 100);
+			
+			JPanel panel = new JPanel();
+			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+			frame.add(panel);
+			
+			JLabel lbl = new JLabel("Select the type of conversion");
+			lbl.setAlignmentX(Component.CENTER_ALIGNMENT);
+			
+			panel.add(lbl);
+			
+			String [] choices = {"Temperature", "Metric", "Currency", "Time Zones", "Imperial"};
+			
+			final JComboBox<String> cb = new JComboBox<String>(choices);
+			
+			cb.setMaximumSize(cb.getPreferredSize()); 
+			cb.setAlignmentX(Component.CENTER_ALIGNMENT);
+			
+			panel.add(cb);
+			
+			JButton btn = new JButton("OK");
+			btn.setAlignmentX(Component.CENTER_ALIGNMENT);
+			panel.add(btn);
+			
+			frame.setVisible(true);
+			
+			btn.addActionListener(new ActionListener()
+				{
+					public void actionPerformed(ActionEvent e)
+					{
+//						frame.getContentPane().removeAll();
+//					    frame.repaint();
+//						frame.getContentPane().setLayout(new FlowLayout());
+						switch(cb.getSelectedIndex())
+						{
+							case 0:
+//								TempConversions.convertTemp();
+//								break;
+//							case 1:
+//								MetricConversions.metricConversions();
+//								break;
+//							case 2:
+//								Currency.converterStart();
+//								break;
+//							case 3: 
+//								TimeZones.takeInput();
+//								break;
+//							case 4:
+//								ImperialConversions.imperialConversion();
+//								break;
+							
+						}
+					}
+				}
+			);
+		}
+		
+	
+		
 
 	}
 
-}
